59f6efe8c50b79d27b3f02e17c1e62e023c1bf4b
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-12-18 16:38:29 -0300

    Add files via upload

---
+++ Lista1POOex2/Lista1POOex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lista1POOex2
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Quadrado quad;
+
+                quad = new Quadrado(23);
+
+                Console.Write("Informe a aresta do quadrado: ");
+                quad.setAresta(int.Parse(Console.ReadLine()));
+                
+                quad.calcularArea();
+
+                Console.WriteLine("A area do quadrado é:{0} ",quad.getArea());
+            }
+
+    }
+}
---
+++ Lista1POOex2/Lista1POOex2/Quadrado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista1POOex2
+{
+    class Quadrado
+    {
+        
+        private int area;
+        private int aresta;
+
+        public Quadrado()
+        {
+            this.area = 0;
+        }
+
+        public Quadrado(int valor)
+        {
+            this.area = valor;
+        }
+        public void setAresta(int p)
+        {
+            aresta = p;
+        } 
+
+        public int getAresta()
+        {
+            return aresta;
+        }
+
+        public int getArea()
+        {
+            return area;
+        }
+
+        public void calcularArea()
+        {
+            area = aresta * aresta;
+        }
+    }
+}
